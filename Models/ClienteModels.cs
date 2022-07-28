1344c8455e2ca6064dca061564887272826ff11e
Cleber Nascimento Silva <cleber_md2@hotmail.com>
2022-07-27 22:32:47 -0300

    Primeiro Commit

---
+++ Models/ClienteModels.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPBank
+{
+    internal class ClienteModels
+    {
+        public string  Nome { get; set; }
+        public decimal Saldo { get; set; }
+
+        
+        public ClienteModels( string nome, decimal saldo)
+        {
+            Nome = nome;
+            Saldo = saldo;
+
+        }
+
+        public ClienteModels()
+        {
+        }
+
+        public override string ToString()
+        {
+            return $"       Status da Conta\nCliente: {Nome} \nSaldo Atual:{Saldo}";
+        }
+
+        public decimal Depositar(decimal saldo)
+        {
+            Console.WriteLine("-----------------------");
+            Console.WriteLine($"-------- nome ------");
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("Quanto Deseja Depositar ?");
+            decimal resp = decimal.Parse(Console.ReadLine());   
+
+            var saldoFinal = saldo + resp;
+            Console.WriteLine($"{saldoFinal}");
+            return saldoFinal;
+        }
+
+        public void Menu ( string nome)
+        {
+            Console.WriteLine("-----------------------");
+            Console.WriteLine($"--------{nome} ------" );
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("Digite 1 para Depositar");
+            Console.WriteLine("Digite 2 para Sacar    ");
+            
+            int resp = int.Parse(Console.ReadLine());
+            switch (resp)
+            {
+                case 1: Depositar(Saldo); break;
+                case 2: Sacar(Saldo); break;
+                case 3: Depositar(Saldo); break;
+
+            }
+
+        }
+
+        public virtual void Sacar(decimal sacar)
+        {
+            Console.WriteLine("-----------------------");
+            Console.WriteLine($"-------- nome ------");
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("Quanto Deseja Sacar?");
+            decimal resp = decimal.Parse(Console.ReadLine());
+
+            var saldoFinal = sacar - resp;
+            Console.WriteLine($"{saldoFinal}");
+           
+            
+            
+        }
+    }
+
+    
+
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using OOPBank;
+using System;
+
+
+
+namespace ContaCorrente
+{
+    public class Program
+    {
+
+
+        public static void Main()
+        {
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("  Digite seu Nome");
+            Console.WriteLine("-----------------------");
+            string nome = Console.ReadLine();
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("Digite seu Saldo");
+            Console.WriteLine("-----------------------");
+            decimal saldo = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("Cadastro Concluido com Sucesso");
+            Console.WriteLine("-----------------------");
+            Console.ReadKey();
+            Console.Clear();
+
+            
+            ClienteModels cliente = new ClienteModels();
+            cliente.Nome = nome;
+            cliente.Saldo = saldo;
+
+            Console.WriteLine(cliente);
+
+            
+            cliente.Menu(nome);
+           
+            Console.WriteLine(cliente);
+
+
+
+        }
+
+        
+           
+        
+
+        
+    }
+}
